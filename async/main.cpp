ee289eeddcce0859d526e5ee3639d1a5ead329e1
Alessandro Diaferia <alediaferia@gmail.com>
2016-03-03 22:49:09 +0000

    Slightly more interesting

--- async/main.cpp
+++ async/main.cpp
@@ -1,16 +1,18 @@
 #include <future>
 #include <iostream>
+#include <vector>
 
-void called_from_async() {
-    std::cout << "Async call" << std::endl;
+int main() {
+  std::vector<std::future<int>> futures;
+  for (int i = 0; i < 10; i++) 
+    futures.push_back(std::future<int>(std::async([](int m) { return m * 2;} , i)));
+
+  std::cout << "Message from main." << std::endl;
+
+  //retrive and print the value stored in the future
+  for (auto &comp : futures)
+    std::cout << "Computed: " << comp.get() << std::endl;
+
+  return 0;
 }
 
-int main(int argc, char *argv[]) {
-    std::future<void> result(std::async(called_from_async));
-
-    std::cout << "int main(): " << "hello!";
-
-    result.get();
-
-    return 0;
-}
